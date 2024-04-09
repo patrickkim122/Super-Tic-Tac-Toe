4bf65b65a2b61574b0afecc4604908d17fb7c04e
Patrick Kim <z5360842@ad.unsw.edu.au>
2024-04-09 17:15:14 +1000

    initial commit

--- src/agent.py
+++ src/agent.py
@@ -7,11 +7,25 @@
 import socket
 import sys
 import numpy as np
+import math
 
 # a board cell can hold:
 #   0 - Empty
 #   1 - We played here
 #   2 - Opponent played here
+
+EMPTY = 2
+
+ILLEGAL_MOVE  = 0
+STILL_PLAYING = 1
+WIN           = 2
+LOSS          = 3
+DRAW          = 4
+
+MAX_MOVE      = 9
+
+MIN_EVAL = -1000000
+MAX_EVAL =  1000000
 
 # the boards are of size 10 because index 0 isn't used
 boards = np.zeros((10, 10), dtype="int8")
@@ -39,6 +53,38 @@ def print_board(board):
     print_board_row(board, 7,8,9,7,8,9)
     print()
 
+# Calling the Minimax algorithm
+def doAlphaBeta(depth, turn):
+    finalScore = []
+    alphaBeta(depth, turn, math.inf, -math.inf)
+    
+def alphaBeta(player, m, board, alpha, beta, best_move):
+
+    best_eval = MIN_EVAL
+
+    if game_won( 1-player, board ):   # LOSS
+        return -1000 + m  # better to win faster (or lose slower)
+
+    this_move = 0
+    for r in range( 1, 10):
+        if board[r] == EMPTY:         # move is legal
+            this_move = r
+            board[this_move] = player # make move
+            this_eval = -alphaBeta(1-player,m+1,board,-beta,-alpha,best_move)
+            board[this_move] = EMPTY  # undo move
+            if this_eval > best_eval:
+                best_move[m] = this_move
+                best_eval = this_eval
+                if best_eval > alpha:
+                    alpha = best_eval
+                    if alpha >= beta: # cutoff
+                        return( alpha )
+
+    if this_move == 0:  # no legal moves
+        return( 0 )     # DRAW
+    else:
+        return( alpha )
+
 # choose a move to play
 def play():
     # print_board(boards)
@@ -57,6 +103,29 @@ def place( board, num, player ):
     global curr
     curr = num
     boards[board][num] = player
+    
+# Check if game is won
+def game_won(p, bd):
+    return(  ( bd[1][0] == p and bd[2][0] == p and bd[3][0] == p )
+           or( bd[4][0] == p and bd[5][0] == p and bd[6][0] == p )
+           or( bd[7][0] == p and bd[8][0] == p and bd[9][0] == p )
+           or( bd[1][0] == p and bd[4][0] == p and bd[7][0] == p )
+           or( bd[2][0] == p and bd[5][0] == p and bd[8][0] == p )
+           or( bd[3][0] == p and bd[6][0] == p and bd[9][0] == p )
+           or( bd[1][0] == p and bd[5][0] == p and bd[9][0] == p )
+           or( bd[3][0] == p and bd[5][0] == p and bd[7][0] == p ))
+    
+# check if one cell is won
+def check_minigame_won( p, bd, curr ):
+    if ( bd[curr][1] == p and bd[curr][2] == p and bd[curr][3] == p )\
+        or ( bd[curr][4] == p and bd[curr][5] == p and bd[curr][6] == p )\
+        or( bd[curr][7] == p and bd[curr][8] == p and bd[curr][9] == p )\
+        or( bd[curr][1] == p and bd[curr][4] == p and bd[curr][7] == p )\
+        or( bd[curr][2] == p and bd[curr][5] == p and bd[curr][8] == p )\
+        or( bd[curr][3] == p and bd[curr][6] == p and bd[curr][9] == p )\
+        or( bd[curr][1] == p and bd[curr][5] == p and bd[curr][9] == p )\
+        or( bd[curr][3] == p and bd[curr][5] == p and bd[curr][7] == p ):
+        bd[curr][0] = p
 
 # read what the server sent us and
 # parse only the strings that are necessary
